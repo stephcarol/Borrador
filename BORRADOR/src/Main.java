e3eab5b6896e92ed2ab37d8ef138c62e304b05bd
stephcarol <scabezas@espol.edu.ec>
2022-06-15 21:32:40 -0400

    subiendo

---
+++ BORRADOR/src/Main.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    LocalDateTime ahora = LocalDateTime.now();
+    System.out.println(ahora.toLocalTime());
+    
+
+    
+  }
+}
